6421472ff49ddc475229df2ee91401686dd605a9
Zahraan Raheel <49128314+DrZaza@users.noreply.github.com>
2020-02-14 17:44:07 -0800

    Search Bar

--- TheNile/src/view_control/AmazonGUI.java
+++ TheNile/src/view_control/AmazonGUI.java
@@ -2,10 +2,19 @@ package view_control;
 
 import control_ap_exam.APexam;
 import java.awt.*;
-import javax.swing.*;
+import javax.swing.*;
 
 public class AmazonGUI extends MainMenu{
 	public AmazonGUI() {
+		getContentPane().setBackground(Color.DARK_GRAY);
+		getContentPane().setLayout(null);
+		
+		TextField textField = new TextField();
+		textField.setText("Search the Nile");
+		textField.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		textField.setForeground(new Color(255, 255, 255));
+		textField.setBackground(new Color(25, 25, 112));
+		textField.setBounds(40, 10, 354, 23);
+		getContentPane().add(textField);
 	}
-
 }
